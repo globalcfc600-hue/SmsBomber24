061cd3000c22c32f319d8f698a9a511c06ce1ed8
globalcfc600-hue <globalcfc600@gmail.com>
2026-02-27 21:27:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -100,4 +100,4 @@ def index():
     return render_template_string(html, phone=state["phone"], logs=state["logs"], running=state["is_running"])
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 50000)))
